2005bc4bb9b744e49425e0509cc6a0ef26e6fb50
SalmaLuna13 <luna.arenas.22046@itsmante.edu.mx>
2025-10-24 01:07:27 -0600

    app actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './Login'
 import Registro from './Registro'
 import AccesosMaestros from './accesosMaestros'
+import Pruebas_temp from './Pruebas_temp'
 import RecuperarContra from './RecuperarContra'
 import EventoCalendario from './EventoCalendario'
 import Calendario from './Calendario'
@@ -11,8 +12,10 @@ import ListaAlumnos from './ListaAlumnos'
 import FichaAlumno from './FichaAlumno'
 import NuevoGrupo from './NuevoGrupo'
 import HomeAlumno from './HomeAlumno'
+import Contacto from './Contacto'
 import Reportes from './Reportes'
 import Mensajes from './Mensajes'
+
 
 
 function App() {
@@ -25,17 +28,19 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/Registro" element={<Registro />} />
         <Route path="/AccesosMaestros" element={<AccesosMaestros />} />
+        <Route path="/Pruebas_temp" element={<Pruebas_temp />} />
         <Route path="/RecuperarContra" element={<RecuperarContra />} />
         <Route path="/EventoCalendario" element={<EventoCalendario />} />
         <Route path="/Calendario" element={<Calendario />} />
         <Route path="/Grupos" element={<Grupos />} />
         <Route path="/alumnos/:matricula/ficha" element={<FichaAlumno />} />
-        <Route path="/Reportes" element={<Reportes />} />
-        <Route path="/Mensajes" element={<Mensajes />} />
         <Route path="/ListaAlumnos" element={<ListaAlumnos />} />
         <Route path="/ListaAlumnos/:idGrupo" element={<ListaAlumnos />} />
         <Route path="/NuevoGrupo" element={<NuevoGrupo />} />
         <Route path="/HomeAlumno/:matricula" element={<HomeAlumno />} />
+        <Route path="/Contacto" element={<Contacto />} />
+        <Route path="/Reportes" element={<Reportes />} />
+        <Route path="/Mensajes" element={<Mensajes />} />
         <Route path="/bitacora/:idSesion" element={<RegistroBitacora />} />
 
       </Routes>
