e4b2745d8af689ce9acc9a094e2fa7e734cd470c
SalmaLuna13 <luna.arenas.22046@itsmante.edu.mx>
2025-10-24 00:54:21 -0600

    app actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import FichaAlumno from './FichaAlumno'
 import NuevoGrupo from './NuevoGrupo'
 import HomeAlumno from './HomeAlumno'
 import Contacto from './Contacto'
+import Reportes from './Reportes'
+import Mensajes from './Mensajes'
+
 
 function App() {
 
@@ -24,17 +27,19 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/Registro" element={<Registro />} />
         <Route path="/AccesosMaestros" element={<AccesosMaestros />} />
-        <Route path="/Pruebas_temp" element={<Pruebas_temp />} />
         <Route path="/RecuperarContra" element={<RecuperarContra />} />
         <Route path="/EventoCalendario" element={<EventoCalendario />} />
         <Route path="/Calendario" element={<Calendario />} />
         <Route path="/Grupos" element={<Grupos />} />
-        <Route path="/FichaAlumno" element={<FichaAlumno />} />
+        <Route path="/alumnos/:matricula/ficha" element={<FichaAlumno />} />
         <Route path="/ListaAlumnos" element={<ListaAlumnos />} />
-        <Route path="/ListaAlumnos/:codigoGrupo" element={<ListaAlumnos />} />
+        <Route path="/ListaAlumnos/:idGrupo" element={<ListaAlumnos />} />
         <Route path="/NuevoGrupo" element={<NuevoGrupo />} />
         <Route path="/HomeAlumno/:matricula" element={<HomeAlumno />} />
         <Route path="/Contacto" element={<Contacto />} />
+        <Route path="/Reportes" element={<Reportes />} />
+        <Route path="/Mensajes" element={<Mensajes />} />
+
 
       </Routes>
 
